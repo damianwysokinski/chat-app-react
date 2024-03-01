77f4d0d506c5d8b28f2ea7489ab2ed1b2667a05f
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-03-01 10:22:31 +0100

    change px-4 to px-2.5 in Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ interface NavbarProps {
 
 const Navbar: FC<NavbarProps> = ({ toggleDrawer }) => {
   return (
-      <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
+      <nav className="bg-white border-b border-gray-200 px-2.5 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
         <div className="flex flex-wrap justify-between items-center">
           <div className="flex justify-start items-center">
             <button
