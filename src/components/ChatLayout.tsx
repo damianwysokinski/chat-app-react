27660b78c956704468fb982de4337ae36ab1c346
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-03-01 14:36:30 +0100

    pass chatRooms data to Sidebar instead of Users data

--- src/components/ChatLayout.tsx
+++ src/components/ChatLayout.tsx
@@ -1,38 +1,34 @@
-"use client";
+'use client';
 
-import { FC, ReactNode, useState } from 'react'
-import { getUsers } from '@/utils/dataUtils';
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar/Sidebar";
+import { FC, ReactNode, useState } from 'react';
+import { getChatrooms, Chatroom } from '@/utils/dataUtils';
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar/Sidebar';
 
 interface ChatLayoutProps {
-    children?: ReactNode;
+  children?: ReactNode;
 }
 
 const ChatLayout: FC<ChatLayoutProps> = ({ children }) => {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const loggedInUserId = 1;
-    const users = getUsers();
-    const filteredUsers = users.filter(user => user.id !== loggedInUserId);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const chatRooms: Chatroom[] = getChatrooms();
 
-    const toggleDrawer = () => {
-        setIsDrawerOpen(!isDrawerOpen);
-    };
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
-    return (
-        <>
-            <div className="antialiased bg-gray-50 dark:bg-gray-900">
-                <Navbar toggleDrawer={toggleDrawer} />
-                <Sidebar users={filteredUsers} isDrawerOpen={isDrawerOpen} />
+  return (
+    <>
+      <div className="bg-gray-50 antialiased dark:bg-gray-900">
+        <Navbar toggleDrawer={toggleDrawer} />
+        <Sidebar chatRooms={chatRooms} isDrawerOpen={isDrawerOpen} />
 
-                <main className="p-4 md:ml-80 min-h-screen pt-20">
-                <div className="flex flex-col justify-between">
-                    {children}
-                </div>
-                </main>
-            </div>
-        </>
-    )
-}
+        <main className="min-h-screen p-4 pt-40 md:ml-80">
+          <div className="flex flex-col justify-between">{children}</div>
+        </main>
+      </div>
+    </>
+  );
+};
 
 export default ChatLayout;
