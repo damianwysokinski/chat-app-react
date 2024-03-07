5005eef81754ba3f481761e4673b3c0d7fe00482
Damian Wysokiński <damian.wysokinski99@gmail.com>
2024-03-07 15:34:28 +0100

    display last message in the sidebar user item

--- src/components/Sidebar/SidebarUserItem.tsx
+++ src/components/Sidebar/SidebarUserItem.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import Link from 'next/link';
-import { Chatroom } from '@/utils/dataUtils';
+import { Chatroom, ChatMessage } from '@/utils/dataUtils';
 import { usePathname } from 'next/navigation';
 
 interface SidebarUserItemProps {
@@ -11,17 +11,24 @@ const SidebarUserItem: FC<SidebarUserItemProps> = ({ user }) => {
   const pathname = usePathname();
   const isLinkActive = pathname === `/chat/${user.id}`;
 
+  const getLastMessage = () => {
+    return user.messages?.[user.messages.length - 1]?.message || '';
+  };
+
+  const lastMessage = getLastMessage();
+
   return (
     <li key={user.id}>
       <Link
         href={`/chat/${user.id}`}
         className={`flex cursor-pointer items-center gap-4 rounded-lg px-2 py-2.5 hover:bg-gray-100 dark:hover:bg-gray-700 ${isLinkActive ? 'bg-gray-100 dark:bg-gray-700' : ''}`}
       >
-        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300">
+        <div className="flex min-h-10 min-w-10 items-center justify-center rounded-full bg-gray-300">
           {user.name.charAt(0)}
         </div>
-        <div className="font-medium dark:text-white">
-          <div>{user.name}</div>
+        <div className="flex flex-col font-medium truncate">
+          <div className="dark:text-white">{user.name}</div>
+          <div className="text-sm text-gray-500 dark:text-gray-400 truncate">{lastMessage}</div>
         </div>
       </Link>
     </li>
